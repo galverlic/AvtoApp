cbd88d2779306cf4f55715d174fb1bffa23e5508
Gal Verlič <galverlic3@gmail.com>
2023-02-15 13:51:33 +0100

    dodan model folder, avto class

---
+++ AvtoApp/Models/Avto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AvtoApp.Models
+{
+    public class Avto
+    {
+
+        public long Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelEconomy { get; set; }
+        public int KilometersDrive { get; set; }
+        public double Power { get; set; }
+    }
+}
+
+
+            
